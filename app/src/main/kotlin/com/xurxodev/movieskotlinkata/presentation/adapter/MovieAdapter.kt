e1494a99eca0bb3bfa967442e9bce9e4207766e5
alex vladimir  minga arias <alexminga.a@gmail.com>
2023-03-01 16:25:20 -0500

    local change Wed Mar  1 16:25:20 -05 2023

--- app/src/main/kotlin/com/xurxodev/movieskotlinkata/presentation/adapter/MovieAdapter.kt
+++ app/src/main/kotlin/com/xurxodev/movieskotlinkata/presentation/adapter/MovieAdapter.kt
@@ -41,4 +41,4 @@ class MovieAdapter(val listener: (Movie) -> Unit) :
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
 
     }
-println(Fri Feb 24 10:52:51 -05 2023) }
+println(Wed Mar 1 16:25:20 -05 2023) }
